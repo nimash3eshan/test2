391b37e15d0b6588fff8c126f45c4c16360a2997
Ishanka Perera <95134409+ishanka020@users.noreply.github.com>
2022-04-30 20:41:46 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,156 +1,32 @@
-#define F_CPU 16000000UL /* Define CPU Frequency 1MHz */
-#include <avr/io.h> /* Include AVR std. library file */
-#include <util/delay.h> /* Include delay header file */
+/*
+ * GccApplication3.c
+ *
+ * Created: 4/30/2022 7:54:26 PM
+ * Author : Ishanka
+ */ 
+#define F_CPU 8000000UL
+#define BAUD 9600
+#define MYUBBR F_CPU/16/BAUD-1
 
-void mixpartStepper();
-void pushingpartStepper();
-void mixpartDCmotor();
 
-int main(void)
-{
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <string.h>
+#include "flowsensor.h"
+
+int main(void){
+	DDRB = DDRB & (~(1<<2));    /*Make PB2 as input pin*/
+	DDRD = DDRD & (~(1<<2));
+	DDRD = DDRD & (~(1<<3));
 	
-	//int period;
-	DDRA=0x00;
-	DDRC = 0xFF; /* Make PORTA lower pins as output */
-	DDRD = 0xFF; //PORTD as Output
-	PORTC=0X00;
-	PORTD = 0x00;
-	PORTA=0XFF;
-	
-	
-	
-	pushingpartStepper();
-	while(1)	{					// Push a tray on to the convey belt
-	if (0!=(PINA &(1<<PA0)))					// IR Sensor detects the tray or not
-	{
-		mixpartStepper();						// stepper motor at mixing position rotates
-		
-		}
-	}
-	
-	
+	DDRB = DDRB | (1<<5);		/* Make PB5 as output pin */
+	DDRB = DDRB | (1<<6);
+	DDRB = DDRB | (1<<7);
+	pump_w();
+	_delay_ms(5000);
+	pump_l();
+	_delay_ms(5000);
+	pump_a();
+	_delay_ms(5000);
 }
 
-void pushingpartStepper()
-{
-	int period= 6;
-	for(int i=0;i<1;i++)
-	{
-		
-		PORTC = 0x09;
-		_delay_ms(period);
-		PORTC = 0x08;
-		_delay_ms(period);
-		PORTC = 0x0C;
-		_delay_ms(period);
-		PORTC = 0x04;
-		_delay_ms(period);
-		PORTC = 0x06;
-		_delay_ms(period);
-		PORTC = 0x02;
-		_delay_ms(period);
-		PORTC = 0x03;
-		_delay_ms(period);
-		PORTC = 0x01;
-		_delay_ms(period);
-	}
-	PORTC = 0x09; /* Last step to initial position */
-	_delay_ms(period);
-	
-	
-	
-	for(int i=0;i<1;i++)
-	{
-		PORTC = 0x01;
-		_delay_ms(period);
-		PORTC = 0x03;
-		_delay_ms(period);
-		PORTC = 0x02;
-		_delay_ms(period);
-		PORTC = 0x06;
-		_delay_ms(period);
-		PORTC = 0x04;
-		_delay_ms(period);
-		PORTC = 0x0C;
-		_delay_ms(period);
-		PORTC = 0x08;
-		_delay_ms(period);
-		PORTC = 0x09;
-		_delay_ms(period);
-		
-	}
-	PORTC = 0x09;
-	_delay_ms(period);
-	_delay_ms(10);
-}
-
-void mixpartStepper()
-{
-	int period=6;
-	for(int i=0;i<2;i++)
-	{
-		
-		PORTC = 0x90;
-		_delay_ms(period);
-		PORTC = 0x80;
-		_delay_ms(period);
-		PORTC = 0xC0;
-		_delay_ms(period);
-		PORTC = 0x40;
-		_delay_ms(period);
-		PORTC = 0x60;
-		_delay_ms(period);
-		PORTC = 0x20;
-		_delay_ms(period);
-		PORTC = 0x30;
-		_delay_ms(period);
-		PORTC = 0x10;
-		_delay_ms(period);
-	}
-	PORTC = 0x90; /* Last step to initial position */
-	_delay_ms(period);
-	
-	//mixpartDCmotor();
-	
-	/* Rotate Stepper Motor Anticlockwise with Full step sequence */
-	for(int i=0;i<2;i++)
-	{
-		PORTC = 0x10;
-		_delay_ms(period);
-		PORTC = 0x30;
-		_delay_ms(period);
-		PORTC = 0x20;
-		_delay_ms(period);
-		PORTC = 0x60;
-		_delay_ms(period);
-		PORTC = 0x40;
-		_delay_ms(period);
-		PORTC = 0xC0;
-		_delay_ms(period);
-		PORTC = 0x80;
-		_delay_ms(period);
-		PORTC = 0x90;
-		_delay_ms(period);
-		
-	}
-	PORTC = 0x90;
-	_delay_ms(period);
-	_delay_ms(10);
-}
-
-void mixpartDCmotor()
-{
-//    for(int i=0; i<1;i++)
-// 	{
-		PORTD = 0x02;
-		_delay_ms(2000);
-		PORTD = 0x03;
-// 		_delay_ms(2000);
-// 		PORTD = 0x01;
-// 		_delay_ms(2000);
-// 		PORTD = 0x00;
-// 		_delay_ms(2000);
-		
-	//}
-	
-}
